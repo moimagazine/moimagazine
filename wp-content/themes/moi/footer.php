28a520043af747a49700369833d17f1dff5a4466
Istvan Pusztai <istvanp@gmail.com>
2014-09-02 03:00:41 -0400

    Cleanup + HP article block test

--- wp-content/themes/moi/footer.php
+++ wp-content/themes/moi/footer.php
@@ -1,4 +1,8 @@
         </article>
+        <div class="clearfix"></div>
+        <footer>
+            &copy; <?php echo date('Y') ?> Moi Magazine
+        </footer>
     </div><!-- #content-inner -->
 </div><!-- #content-outer -->
 </body>
