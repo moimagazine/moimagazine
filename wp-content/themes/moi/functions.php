ccb8e9e3e3a287d0c5a1c999559fadd844a84745
Istvan Pusztai <istvanp@gmail.com>
2014-09-06 00:46:40 -0400

    Added new image sizes

--- wp-content/themes/moi/functions.php
+++ wp-content/themes/moi/functions.php
@@ -31,10 +31,23 @@ function moi_setup() {
     ));
 
     add_theme_support('post-thumbnails');
+
+    add_image_size('featured', 800, 400, true);
+    add_image_size('vertical', 300, 375, true);
 }
 
 add_action('wp_enqueue_scripts', 'moi_scripts');
 
 function moi_scripts() {
     wp_enqueue_style('moi-style', get_stylesheet_uri());
+}
+
+add_filter('image_size_names_choose', 'moi_image_sizes');
+
+function moi_image_sizes($presets) {
+    $custom = array(
+        "featured" => __("Featured Image"),
+        "vertical" => __("Vertical Image")
+    );
+    return array_merge($presets, $custom);
 }
