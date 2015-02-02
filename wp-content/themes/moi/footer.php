0136a261b02bf234b0e9b5c236d6556961654a6e
Istvan Pusztai <istvanp@gmail.com>
2015-02-02 00:15:12 -0500

    Integrate footer

--- wp-content/themes/moi/footer.php
+++ wp-content/themes/moi/footer.php
@@ -1,16 +1,30 @@
         <div class="pull-right">
             <img src="http://placehold.it/728x90&text=Leaderboard+(728x90)" width="728" height="90" />
         </div>
+        </article>
         <h2 class="instagram section-heading">Instagram Feed</h2>
         <div class="instagram-feed">
             <?php echo do_shortcode('[instagram-feed]'); ?>
         </div>
-        </article>
         <div class="clearfix"></div>
-        <footer>
-            &copy; <?php echo date('Y') ?> Moi Magazine
-        </footer>
     </div><? #content-inner ?>
 </div><? #content-outer ?>
+<footer>
+    <div id="footer-outer" class="page-width">
+        <div id="footer-inner">
+            <ul>
+                <li><a href="/">About Moi</a></li>
+                <li><a href="/">The Team</a></li>
+                <li><a href="/">Careers</a></li>
+                <li><a href="/">Press</a></li>
+                <li><a href="/">Help &amp; FAQ</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="footer-bottom">
+        All Rights Reserved &copy; <?php echo date('Y') ?> <strong>Moi Magazine</strong>
+    </div>
+</footer>
 </body>
 </html>
